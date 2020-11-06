49248f1ee988a4703125b09b0f682c27a40a40c9
Victoria Maziero <victoriamaziero@alunos.utfpr.edu.br>
2020-11-06 01:08:40 -0300

    Add botão para excluir produtos do banco.

---
+++ excluiProduto.php
@@ -0,0 +1,21 @@
+<?php
+	//Connection
+	include_once("conecta.php");
+
+	//Receiving data
+	$recId=$_POST["id"];
+	
+	//Delete categories for the product
+	$query="DELETE FROM productCategory WHERE idProduct=$recId";
+	mysqli_query($connection, $query);
+
+	//Delete product on database
+	$query="DELETE FROM product WHERE id=$recId";
+	mysqli_query($connection, $query);
+
+	header("location: formExcluiProduto.php");
+?>
+
+
+
+
---
+++ formExcluiProduto.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>TechStart Pro | Olist</title>
+<link href="../css/estilo.css" rel="stylesheet">
+</head>
+
+<body>
+    
+	<center>
+		<h3>Listagem de produtos</h3>
+        <?php
+					//Connection
+					include_once("./conecta.php");
+					//Get all categories from database
+					$data=mysqli_query($connection, "SELECT * FROM product");
+                                        
+                    echo "<table border=1>";
+                    echo "<tr>
+                            <th>Nome</th>
+                            <th>Descrição</th>
+                            <th>Valor</th>
+                            <th>Categorias</th>
+                            <th>Apagar</th>
+                        </tr>";
+
+					while($row=$data->fetch_array()){
+                        echo "<tr>";
+                        echo "<td>$row[name]</td>";
+                        echo "<td>$row[description]</td>";
+                        echo "<td>$row[value]</td>";
+                        $categoryData=mysqli_query($connection, "SELECT * FROM productCategory PC INNER JOIN category C ON C.id=PC.idCategory WHERE PC.idProduct=$row[id]");
+                        echo "<td>";                    
+                        while($categoryRow=$categoryData->fetch_array()){
+                            echo "$categoryRow[name], ";
+                        }
+                        echo "</td>";
+
+                        echo "<td><form method='post' action='excluiProduto.php' class='form' style='width:30%;'>
+                                <input type='hidden' value='$row[id]' name='id' id='id'>
+                                <input type='submit' value='Excluir produto' class='button'>
+                            </form></td>";
+                        echo "</tr>";	
+                    }
+                    echo "</table>";
+
+                    
+				?>
+	</center>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
