560511e869deee8b1873018d8c50122bbbbf8e13
Victoria Maziero <victoriamaziero@alunos.utfpr.edu.br>
2020-11-06 03:38:32 -0300

    Add filtros de pesquisa.

--- formEditaProduto.php
+++ formEditaProduto.php
@@ -8,7 +8,7 @@
 
 <body>
 	<center>
-		<h3>Cadastro de Produto</h3>
+		<h3>Editar produto</h3>
 			<h4>Selecione novamente a(s) categoria(s) desejada(s) pressionando a tecla Ctrl e clicando sobre as opções.</h4>
 			<?php 
 				//Connection
@@ -21,25 +21,36 @@
 
 			?>
 			<form method="post" action="editaProduto.php" class="form" style="width:30%;">
-			<input type='hidden' value="<?php echo $id ?>" name='id' id='id'>
-			<input type="text" name="name" required class="field" value="<?php echo $productInfo['name']; ?>">
-			<input type="textarea" rows="4" cols="20" name="description" required class="field" value="<?php echo $productInfo['description']; ?>">
-			<input type="number" step="any" name="value" required class="field" value="<?php echo $productInfo['value']; ?>">
-			<select name="category[]" id="category" multiple>
-				<option value="" disabled selected>Categorias</option>
-				<?php
-					//Get all categories from database
-					$data=mysqli_query($connection, "SELECT * FROM category");
-					//Separando os dados com Array
-					
-					while($row=$data->fetch_array()){
-						echo "<option value='$row[id]'>$row[name]</option>";	
-					}
-					
-				?>
-			</select>
+			<table>
+			<tr>
+				<th></th>
+				<th>Nome</th>
+				<th>Descrição</th>
+				<th>Valor</th>
+				<th>Categorias</th>
+			</tr>
+			<tr>
+				<td><input type='hidden' value="<?php echo $id ?>" name='id' id='id'></td>
+				<td><input type="text" name="name" required class="field" value="<?php echo $productInfo['name']; ?>"></td>
+				<td><input type="textarea" rows="4" cols="20" name="description" required class="field" value="<?php echo $productInfo['description']; ?>"></td>
+				<td><input type="number" step="any" name="value" required class="field" value="<?php echo $productInfo['value']; ?>"></td>
+				<td><select name="category[]" id="category" multiple>
+					<option value="" disabled selected>Categorias</option>
+					<?php
+						//Get all categories from database
+						$data=mysqli_query($connection, "SELECT * FROM category");
+						//Separando os dados com Array
+						
+						while($row=$data->fetch_array()){
+							echo "<option value='$row[id]'>$row[name]</option>";	
+						}
+						
+					?>
+				</select></td>
 
-			<input type="submit" value="Editar produto" class="button">
+				<td><input type="submit" value="Salvar alterações" class="button"></td>
+			</tr>
+			</table>
 		</form>
 	</center>
 </body>
--- listaProdutos.php
+++ listaProdutos.php
@@ -9,12 +9,66 @@
 <body>
     
 	<center>
+        <form method="post" action="listaProdutos.php" class="form" style="width:30%;">
+			<input type='hidden'  name='id' id='id'>
+			<input type="text" name="name" class="field">
+			<input type="textarea" rows="4" cols="20" name="description" class="field">
+			<input type="number" step="any" name="value" class="field">
+			<select name="category[]" id="category" multiple>
+				<option value="" disabled selected>Categorias</option>
+                <?php
+                     //Connection
+					include_once("./conecta.php");
+
+					//Get all categories from database
+					$data=mysqli_query($connection, "SELECT * FROM category");
+					//Separando os dados com Array
+					
+					while($row=$data->fetch_array()){
+						echo "<option value='$row[id]'>$row[name]</option>";	
+					}
+					
+				?>
+			</select>
+
+			<input type="submit" value="Pesquisar" class="button">
+		</form>
 		<h3>Listagem de produtos</h3>
         <?php
-					//Connection
-					include_once("./conecta.php");
-					//Get all categories from database
-					$data=mysqli_query($connection, "SELECT * FROM product");
+                    //Get all categories from database
+                    if(empty($_POST)){
+                        $data=mysqli_query($connection, "SELECT * FROM product");
+                    }else{
+                        $where="";
+                        if($_POST["name"]){
+                            $name=$_POST["name"];
+                            $where=$where." AND name LIKE '%$name%'";
+                        }
+                        if($_POST["description"]){
+                            $description=$_POST["description"];
+                            $where=$where." AND description LIKE '%$description%'";
+                        }
+                        if($_POST["value"]){
+                            $value=$_POST["value"];
+                            $where=$where." AND value='$value'";
+                        }
+                        if(isset($_POST["category"])){
+                            $where=$where." AND ";
+                            $whereCategory="";
+                            foreach ($_POST["category"] as $category){
+                                $whereCategory=$whereCategory." OR PC.idCategory='$category'";
+                            }
+                            // Remove first "OR"
+                            $whereCategory=substr($whereCategory, 4);
+                            $where=$where.$whereCategory;
+                        }
+                        // Remove first "AND"
+                        $where=substr($where, 5);
+                        $query=" SELECT * FROM product P INNER JOIN productCategory PC ON PC.idProduct=P.id WHERE $where";
+                        $data=mysqli_query($connection, $query);
+
+                    }
+					
                                         
                     echo "<table border=1>";
                     echo "<tr>
